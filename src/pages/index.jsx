96c06b6cd2208752b4f344a30b452c50515ef16e
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-12-10 11:39:15 +0900

    :egg: クリックイベントの実験的追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,17 @@
 import Head from 'next/head'
+import { useCallback } from 'react'
 import { Footer } from 'src/components/Footer/Footer'
 import { Header } from 'src/components/Header/Header'
 import { Main } from 'src/components/Main/Main'
 import styles from 'src/styles/Home.module.css'
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    e.preventDefault()
+    console.log(e.target)
+    alert(123)
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +22,7 @@ export default function Home() {
         />
       </Head>
       <Header />
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
